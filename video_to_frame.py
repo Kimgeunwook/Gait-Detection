b127ce9605d371adbb554bb708e50ab446ea86d0
Kimgeunwook <48399897+Kimgeunwook@users.noreply.github.com>
2021-10-23 22:43:35 +0900

    Update video_to_frame.py

--- video_to_frame.py
+++ video_to_frame.py
@@ -2,47 +2,6 @@ import cv2
 import os
 import argparse
 
-# Read the video from specified path
-# dir = '/home/ugh/AlphaPose/200305_gait/'
-# list = os.listdir(dir)
-# for d in list:
-#     cam = cv2.VideoCapture(dir+d)
-#     try:
-#
-#         # creating a folder named data
-#         if not os.path.exists('frame_data200305/'+d[:-4]):
-#             os.makedirs('frame_data200305/'+d[:-4])
-#
-#         # if not created then raise error
-#     except OSError:
-#         print('Error: Creating directory of data')
-#
-#     # frame
-#     currentframe = 0
-#
-#     while (True):
-#
-#         # reading from frame
-#         ret, frame = cam.read()
-#
-#         if ret:
-#             # if video is still left continue creating images
-#             name = './frame_data200305/'+d[:-4]+'/'+ str(currentframe) + '.jpg'
-#             print('Creating...' + name)
-#
-#             # writing the extracted images
-#             cv2.imwrite(name, frame)
-#
-#             # increasing counter so that it will
-#             # show how many frames are created
-#             currentframe += 1
-#         else:
-#             break
-#
-# # Release all space and windows once done
-# cam.release()
-# cv2.destroyAllWindows()
-
 
 def frame(input_dir):
 
@@ -82,10 +41,7 @@ def frame(input_dir):
         print('Creating...' + str(count)+'_back.png')
         cv2.imwrite(name, frame)
         count += 1
-
-    # Release all space and windows once done
     video.release()
     video2.release()
-    #cv2.destroyAllWindows()
 
     return
